777914d791b2c0df39a4bd40747bbc0ce7c52f2c
Siddartha Pothapragada <spotha@codeaurora.org>
2011-10-11 18:48:49 -0700

    Telephony: Fix to address when EF-IMG raw data is wrong.
    CRs-Fixed: 309373
    
    Change-Id: I353cdcdb57fb0bbdadc467b518e3b1fff5f6f69f

--- telephony/java/com/android/internal/telephony/cat/IconLoader.java
+++ telephony/java/com/android/internal/telephony/cat/IconLoader.java
@@ -156,6 +156,9 @@ class IconLoader extends Handler {
                 } else if (mId.codingScheme == ImageDescriptor.CODING_SCHEME_COLOUR) {
                     mIconData = rawData;
                     readClut();
+                } else {
+                    // post null icon back to the caller.
+                    postIcon();
                 }
                 break;
             case EVENT_READ_CLUT_DONE:
