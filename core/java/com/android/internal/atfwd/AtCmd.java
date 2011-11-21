765dace6e152d255cc48f0b18acc6f4e78fafbf7
Uma Maheswari Ramalingam <umam@codeaurora.org>
2011-11-21 12:43:01 -0800

    frameworks/base: Additional atfwd commands support
    1) Added common AtCmd error codes and OpCodes
    
    2) frameworks/base: Implementation of AT+CRSL, AT+CSS
    
    Following commands are supported
    CRSL:
        +CRSL?
        +CRSL=?
        +CRSL=<level>
    CSS:
        +CSS
    
    Reference spec : 3GPP TS 27.007 V9.1.0
    
    3) Support for AT +CMAR=<pin_lock_code> & +CMAR=?
    
    Reference spec 3GPP TS 27.007.
    
    Implementation for scenarios not explicitly covered in spec
    
     - Specs does not specify an option for SD card erase.
       Implementation has an additional argument in CMAR command
       for this.
    
     - Specs does not support pattern lock pin code
       Phone will throw error if it is pattern locked.
    
     - Spec mandates CMAR pin_lock_code verification
       If numeric/alphanumeric password is not set in phone ,
       or if lock code is not provided in CMAR,
       phone will throw error.
    
    Change-Id: Iebadf6236cada7f01dd09b58dfde2a20000829f3

--- core/java/com/android/internal/atfwd/AtCmd.java
+++ core/java/com/android/internal/atfwd/AtCmd.java
@@ -39,9 +39,27 @@ public class AtCmd implements Parcelable {
     public static final int AT_OPCODE_QU = 4;
     public static final int AT_OPCODE_AR = 8;
 
-    private int mOpcode;
+    // AT_OPCODE_NA | AT_OPCODE_QU
+    public static final int ATCMD_OPCODE_NA_QU    = 5;
+    // AT_OPCODE_NA | AT_OPCODE_EQ | AT_OPCODE_QU
+    public static final int ATCMD_OPCODE_NA_EQ_QU = 7;
+    // AT_OPCODE_NA | AT_OPCODE_EQ | AT_OPCODE_AR
+    public static final int ATCMD_OPCODE_NA_EQ_AR = 11;
+
+    // Error codes are defined as per 3GPP spec TS 27.007, Section 9.2.1.
+    public static final int AT_ERR_PHONE_FAILURE      = 0;
+    public static final int AT_ERR_NO_CONN_TO_PHONE   = 1;
+    public static final int AT_ERR_OP_NOT_ALLOW       = 3;
+    public static final int AT_ERR_OP_NOT_SUPP        = 4;
+    public static final int AT_ERR_INCORRECT_PASSWORD = 16;
+    public static final int AT_ERR_NOT_FOUND          = 22;
+    public static final int AT_ERR_INVALID_CHARS      = 25;
+    public static final int AT_ERR_INCORRECT_PARAMS   = 50;
+    public static final int AT_ERR_UNKNOWN            = 100;
+
     private String mName;
     private String mTokens[];
+    private int mOpcode;
 
     public int getOpcode() {
         return mOpcode;
@@ -103,6 +121,50 @@ public class AtCmd implements Parcelable {
         }
         ret += "}";
         return ret;
+    }
+
+    public String getAtCmdErrStr(int errCode) {
+
+        /* Per 3GPP spec TS 27.007, section 9.2 states as follows :
+         * "The format of <err> can be either numeric or verbose"
+         * By default this method sends the error result in numeric format.
+         * TBD :Consider cmee val (0,1,2) before sending relevant error string
+         *      Note that cmee_val is treated as 1 while sending error strings
+         */
+        String errStrResult = "+CME ERROR: 100"; //default err string
+
+        switch(errCode) {
+            case AT_ERR_PHONE_FAILURE:
+                errStrResult = "+CME ERROR: 0";
+                break;
+            case AT_ERR_NO_CONN_TO_PHONE:
+                errStrResult = "+CME ERROR: 1";
+                break;
+            case AT_ERR_OP_NOT_ALLOW:
+                errStrResult = "+CME ERROR: 3";
+                break;
+            case AT_ERR_OP_NOT_SUPP:
+                errStrResult = "+CME ERROR: 4";
+                break;
+            case AT_ERR_INCORRECT_PASSWORD:
+                errStrResult = "+CME ERROR: 16";
+                break;
+            case AT_ERR_NOT_FOUND:
+                errStrResult = "+CME ERROR: 22";
+                break;
+            case AT_ERR_INVALID_CHARS:
+                errStrResult = "+CME ERROR: 25";
+                break;
+            case AT_ERR_INCORRECT_PARAMS:
+                errStrResult = "+CME ERROR: 50";
+                break;
+            case AT_ERR_UNKNOWN:
+                break;
+            default :
+                break;
+        }
+
+        return errStrResult;
     }
 
     public static final Parcelable.Creator<AtCmd> CREATOR = new Parcelable.Creator<AtCmd>() {
