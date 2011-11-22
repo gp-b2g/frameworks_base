Jackeagle/caf_frameworks_base

performance: Add Performance class
New Performance class to allow system CPU control

Change-Id: Ibd124c690acb9c6066c3e2f95a34f9663c027002

core/jni/Android.mk
@@ -155,6 +155,7 @@ LOCAL_SRC_FILES:= \
 	android_backup_BackupHelperDispatcher.cpp \
 	android_content_res_ObbScanner.cpp \
 	com_android_internal_app_ActivityTrigger.cpp \
+	org_codeaurora_Performance.cpp \
     android_content_res_Configuration.cpp \
     android_hardware_fm.cpp
 

gp-b2g/frameworks_base

Change-Id: Ie476aefcec7fc20cab8d747ef7c3994b26fcbb62

core/jni/Android.mk
@@ -155,7 +155,8 @@ LOCAL_SRC_FILES:= \
 	android_content_res_ObbScanner.cpp \
 	android_content_res_Configuration.cpp \
 	android_animation_PropertyValuesHolder.cpp \
-	com_android_internal_app_ActivityTrigger.cpp
+	com_android_internal_app_ActivityTrigger.cpp \
+	org_codeaurora_Performance.cpp
 
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \