Jackeagle/caf_frameworks_base

ActivityTrigger:  New class to invoke when activity starts/resumes
Change-Id: Ia9e08a2cbf5c003b05f6fc4d20352c37d5fec0e7

core/jni/Android.mk
@@ -158,6 +158,7 @@ LOCAL_SRC_FILES:= \
 	android_backup_FileBackupHelperBase.cpp \
 	android_backup_BackupHelperDispatcher.cpp \
 	android_content_res_ObbScanner.cpp \
+	com_android_internal_app_ActivityTrigger.cpp \
     android_content_res_Configuration.cpp \
     android_hardware_fm.cpp
 

gp-b2g/frameworks_base

Change-Id: I99cb7ea97ad983ce4ba121635186e9f69fabd512

core/jni/Android.mk
@@ -154,7 +154,8 @@ LOCAL_SRC_FILES:= \
 	android_app_backup_FullBackup.cpp \
 	android_content_res_ObbScanner.cpp \
 	android_content_res_Configuration.cpp \
-    android_animation_PropertyValuesHolder.cpp
+	android_animation_PropertyValuesHolder.cpp \
+	com_android_internal_app_ActivityTrigger.cpp
 
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \
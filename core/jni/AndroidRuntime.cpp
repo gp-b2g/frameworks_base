21fdf5be7e55d04307a89f691d1f2ba45d436eea
David Ng <dave@codeaurora.org>
2011-07-14 10:49:26 -0700

    ActivityTrigger:  New class to invoke when activity starts/resumes
    Change-Id: I99cb7ea97ad983ce4ba121635186e9f69fabd512

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -176,6 +176,7 @@ extern int register_android_content_res_ObbScanner(JNIEnv* env);
 extern int register_android_content_res_Configuration(JNIEnv* env);
 extern int register_android_animation_PropertyValuesHolder(JNIEnv *env);
 extern int register_com_android_internal_content_NativeLibraryHelper(JNIEnv *env);
+extern int register_com_android_internal_app_ActivityTrigger(JNIEnv *env);
 
 static AndroidRuntime* gCurRuntime = NULL;
 
@@ -1201,6 +1202,8 @@ static const RegJNIRec gRegJNI[] = {
 
     REG_JNI(register_android_animation_PropertyValuesHolder),
     REG_JNI(register_com_android_internal_content_NativeLibraryHelper),
+
+    REG_JNI(register_com_android_internal_app_ActivityTrigger),
 };
 
 /*
