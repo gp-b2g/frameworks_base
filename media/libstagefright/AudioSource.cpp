46b98496814b22a6cb87197a9369eadf866c69c9
Karthik Gunda <gunda@codeaurora.org>
2011-12-22 15:29:43 +0530

    libstagefright: Fix for av sync issue with tunnel audio
    - Observed av sync issue for tunnel audio as timestamp
      is not getting updated properly for tunnel audio.
    - Fix this by updating timestamp and drifttime properly.
    
    Change-Id: I8a85f2575738260bbe4c55eb42908c2a3fa1e76d

--- media/libstagefright/AudioSource.cpp
+++ media/libstagefright/AudioSource.cpp
@@ -87,6 +87,7 @@ AudioSource::AudioSource(
 AudioSource::AudioSource( int inputSource, const sp<MetaData>& meta )
     : mStarted(false),
       mPrevSampleTimeUs(0),
+      mNumFramesReceived(0),
       mNumClientOwnedBuffers(0),
       mFormat(AUDIO_FORMAT_PCM_16_BIT),
       mMime(MEDIA_MIMETYPE_AUDIO_RAW) {
@@ -100,7 +101,6 @@ AudioSource::AudioSource( int inputSource, const sp<MetaData>& meta )
     CHECK( meta->findInt32( kKeySampleRate, &sampleRate ) );
     int32_t frameSize = -1;
     mSampleRate = sampleRate;
-    //mNumChannels = channels;
     if ( !strcasecmp( mime, MEDIA_MIMETYPE_AUDIO_AMR_NB ) ) {
         mFormat = AUDIO_FORMAT_AMR_NB;
         frameSize = AMR_FRAMESIZE;
@@ -280,10 +280,11 @@ status_t AudioSource::read(
 
     // Mute/suppress the recording sound
 
+    int64_t timeUs;
+    CHECK(buffer->meta_data()->findInt64(kKeyTime, &timeUs));
+    int64_t elapsedTimeUs = timeUs - mStartTimeUs;
+
     if ( mFormat == AUDIO_FORMAT_PCM_16_BIT ) {
-        int64_t timeUs;
-        CHECK(buffer->meta_data()->findInt64(kKeyTime, &timeUs));
-        int64_t elapsedTimeUs = timeUs - mStartTimeUs;
         if (elapsedTimeUs < kAutoRampStartUs) {
             memset((uint8_t *) buffer->data(), 0, buffer->range_length());
         } else if (elapsedTimeUs < kAutoRampStartUs + kAutoRampDurationUs) {
@@ -298,6 +299,7 @@ status_t AudioSource::read(
                     (uint8_t *) buffer->data(), buffer->range_length());
         }
     }
+
     // Track the max recording signal amplitude.
     if (mTrackMaxAmplitude && ( mFormat == AUDIO_FORMAT_PCM_16_BIT)) {
         trackMaxAmplitude(
@@ -374,14 +376,30 @@ status_t AudioSource::dataCallbackTimestamp(
     }
 
     buffer->set_range(0, bufferSize);
-    timestampUs += ((1000000LL * (bufferSize / (2 * mRecord->channelCount()))) +
+
+    int64_t recordDurationUs = 0;
+    if ( mFormat == AUDIO_FORMAT_PCM_16_BIT ){
+        recordDurationUs = ((1000000LL * (bufferSize / (2 * mRecord->channelCount()))) +
                     (mSampleRate >> 1)) / mSampleRate;
+    } else {
+       recordDurationUs = bufferDurationUs(bufferSize);
+    }
+    timestampUs += recordDurationUs;
+
 
     if (mNumFramesReceived == 0) {
         buffer->meta_data()->setInt64(kKeyAnchorTime, mStartTimeUs);
     }
     buffer->meta_data()->setInt64(kKeyTime, mPrevSampleTimeUs);
-    buffer->meta_data()->setInt64(kKeyDriftTime, timeUs - mInitialReadTimeUs);
+    if (mFormat == AUDIO_FORMAT_PCM_16_BIT) {
+        buffer->meta_data()->setInt64(kKeyDriftTime, timeUs - mInitialReadTimeUs);
+    }
+    else {
+        int64_t wallClockTimeUs = timeUs - mInitialReadTimeUs;
+        int64_t mediaTimeUs = mStartTimeUs + mPrevSampleTimeUs;
+        buffer->meta_data()->setInt64(kKeyDriftTime, mediaTimeUs - wallClockTimeUs);
+    }
+
     mPrevSampleTimeUs = timestampUs;
     mNumFramesReceived += buffer->range_length() / sizeof(int16_t);
     mBuffersReceived.push_back(buffer);
