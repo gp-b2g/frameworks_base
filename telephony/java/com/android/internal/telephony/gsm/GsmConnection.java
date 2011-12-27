b006bd679ef4ebb74353a281a4160fbf8f01eb67
Uma Maheswari Ramalingam <umam@codeaurora.org>
2011-12-27 15:02:49 -0800

    Telephony: Fix warning in comment
    Change-Id: I8566a91f5cca9392bd30ed89b730087c3738b298

--- telephony/java/com/android/internal/telephony/gsm/GsmConnection.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmConnection.java
@@ -547,18 +547,18 @@ public class GsmConnection extends Connection {
         if (PhoneNumberUtils.is12Key(c)) {
             owner.cm.sendDtmf(c, h.obtainMessage(EVENT_DTMF_DONE));
         } else if (c == PhoneNumberUtils.PAUSE) {
-            // From TS 22.101:
-            // It continues...
-            // Upon the called party answering the UE shall send the DTMF digits
-            // automatically to the network after a delay of 3 seconds(± 20 %).
-            // The digits shall be sent according to the procedures and timing
-            // specified in 3GPP TS 24.008 [13]. The first occurrence of the
-            // "DTMF Control Digits Separator" shall be used by the ME to
-            // distinguish between the addressing digits (i.e. the phone number)
-            // and the DTMF digits. Upon subsequent occurrences of the
-            // separator,
-            // the UE shall pause again for 3 seconds (± 20 %) before sending
-            // any further DTMF digits.
+            /*
+             * From TS 22.101: It continues... Upon the called party answering
+             * the UE shall send the DTMF digits automatically to the network
+             * after a delay of 3 seconds(plus/minus 20 %). The digits shall be
+             * sent according to the procedures and timing specified in 3GPP TS
+             * 24.008 [13]. The first occurrence of the
+             * "DTMF Control Digits Separator" shall be used by the ME to
+             * distinguish between the addressing digits (i.e. the phone number)
+             * and the DTMF digits. Upon subsequent occurrences of the
+             * separator, the UE shall pause again for 3 seconds before sending
+             * any further DTMF digits.
+             */
             h.sendMessageDelayed(h.obtainMessage(EVENT_PAUSE_DONE),
                     PAUSE_DELAY_MILLIS);
         } else if (c == PhoneNumberUtils.WAIT) {
