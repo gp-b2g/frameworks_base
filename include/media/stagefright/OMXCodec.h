b12ebf43f6ad4eeb998d4e0357a55d4b3b5053f7
Karthik Gunda <gunda@codeaurora.org>
2012-01-11 17:38:10 +0530

    libstagefright: Configure the g711 decoder with parser sample rate
    - a-law and u-law pcm clips of sample rate other than 8000 does not
      play properly.
    - Sample rate in g711 decoder is hardcoded as 8000.
    - Configure sample rate in g711 decoder as reported by parser.
    
    Change-Id: I02044f91ca0fde6ed1f2d812def61733656fc251
    CRs-Fixed: 329958

--- include/media/stagefright/OMXCodec.h
+++ include/media/stagefright/OMXCodec.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
- * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 /*--------------------------------------------------------------------------
-Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+Copyright (c) 2012, Code Aurora Forum. All rights reserved.
 --------------------------------------------------------------------------*/
 
 #ifndef OMX_CODEC_H_
@@ -254,7 +254,7 @@ private:
     void setAMRFormat(bool isWAMR, int32_t bitRate);
     status_t setAACFormat(int32_t numChannels, int32_t sampleRate, int32_t bitRate);
     void setEVRCFormat( int32_t sampleRate, int32_t numChannels, int32_t bitRate);
-    void setG711Format(int32_t numChannels);
+    void setG711Format(int32_t numChannels, int32_t sampleRate);
     void setQCELPFormat( int32_t sampleRate, int32_t numChannels, int32_t bitRate);
 
     status_t setVideoPortFormatType(
--- media/libstagefright/codecs/g711/dec/SoftG711.h
+++ media/libstagefright/codecs/g711/dec/SoftG711.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +48,7 @@ private:
 
     bool mIsMLaw;
     OMX_U32 mNumChannels;
+    OMX_U32 mSampleRate;
     bool mSignalledError;
 
     void initPorts();
