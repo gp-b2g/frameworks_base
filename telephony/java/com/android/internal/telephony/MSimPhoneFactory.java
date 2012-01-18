561c16e0af26a2750e60f6da37c147e064ae60f7
Suresh Koleti <skolet@codeaurora.org>
2012-01-18 18:53:13 +0530

    Telephony (DSDS): Remove Redundant logging
    Change-Id: I069934a13d4d7de23f6108fb10e497484d7131ed

--- telephony/java/com/android/internal/telephony/MSimPhoneFactory.java
+++ telephony/java/com/android/internal/telephony/MSimPhoneFactory.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (c) 2011 Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011-2012 Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -221,7 +221,6 @@ public class MSimPhoneFactory extends PhoneFactory {
             subscription = Settings.System.getInt(sContext.getContentResolver(),
                     Settings.System.DEFAULT_SUBSCRIPTION);
         } catch (SettingNotFoundException snfe) {
-            Log.e(LOG_TAG, "Settings Exception Reading Default Subscription");
         }
 
         return subscription;
