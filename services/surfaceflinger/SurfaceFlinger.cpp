aa15a84003562f2f00660aa05f8baacfd769a425
Kyong Hwa Bae <kbae@codeaurora.org>
2012-03-07 17:14:36 -0800

    SurfaceFlinger: Skip composition when rotation is not completed
    When rotating, each layer's buffer is already changed to
    new orientation before SurfaceFlinger gets the signal from
    Window Manager. If the orientations of all the buffers are
    changed to new orientation, then that's fine. But, sometimes,
    only one buffer is changed and others remain the same.
    In this case, skip composition.
    
    Change-Id: I850fe8ee6d806e21833e6811f95878fcfb6f7c40
    CRs-fixed: 338034

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -442,6 +442,11 @@ bool SurfaceFlinger::threadLoop()
         handleWorkList();
     }
 
+    if (isRotationCompleted() == false) {
+        LOGD("Rotation is not finished. Skip the composition");
+        return true;
+    }
+
     const DisplayHardware& hw(graphicPlane(0).displayHardware());
     if (LIKELY(hw.canDraw())) {
 #ifdef GFX_TESTFRAMEWORK
@@ -843,6 +848,19 @@ void SurfaceFlinger::unlockPageFlip(const LayerVector& currentLayers)
         const sp<LayerBase>& layer(layers[i]);
         layer->unlockPageFlip(planeTransform, mDirtyRegion);
     }
+}
+
+bool SurfaceFlinger::isRotationCompleted()
+{
+    const Vector< sp<LayerBase> >& currentLayers(mVisibleLayersSortedByZ);
+    const size_t count = currentLayers.size();
+
+    for (size_t i=0 ; i<count ; i++) {
+        if (currentLayers[i]->isRotated() == false) {
+            return false;
+        }
+    }
+    return true;
 }
 
 void SurfaceFlinger::handleWorkList()
