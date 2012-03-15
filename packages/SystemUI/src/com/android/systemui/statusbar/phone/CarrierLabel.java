46c56e33dffede7a81894ea90c9802ff993fb901
liang fei <fliang@codeaurora.org>
2012-03-15 14:36:42 +0800

    CAF:QRD_QI:BUGFIX:None:334974:modify the issue that the status bar show only one carrier name
    	Component Name: BUGFIX
    	Feature Name: None
    	Fixed CRs: 334974
    	Targets: msm7627A_sku3
    	Required VUs:
    	Dependency Notes:
    	Limitations:
    	Suggested Testing:
    	Peer Review:
    	Reference Build or VU:
    	Comments: zhou jian
    
    Change-Id: Iba8d771004a25edb71d2dea3891dd806cc6e9432

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CarrierLabel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CarrierLabel.java
@@ -21,6 +21,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.TypedArray;
 import android.provider.Telephony;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -33,13 +34,16 @@ import java.util.Calendar;
 import java.util.TimeZone;
 
 import com.android.internal.R;
+import com.android.internal.telephony.MSimConstants;
 
 /**
  * This widget display an analogic clock with two hands for hours and
  * minutes.
  */
 public class CarrierLabel extends TextView {
+    private static final int DEFAULT_SUB = 0;
     private boolean mAttached;
+    private int mSubscription;
 
     public CarrierLabel(Context context) {
         this(context, null);
@@ -51,6 +55,11 @@ public class CarrierLabel extends TextView {
 
     public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                com.android.systemui.R.styleable.CarrierLabel, 0, R.style.Widget_TextView);
+        mSubscription = a.getInt(com.android.systemui.R.styleable.CarrierLabel_subscription, DEFAULT_SUB);
+        a.recycle();
+
         updateNetworkName(false, null, false, null);
     }
 
@@ -79,11 +88,15 @@ public class CarrierLabel extends TextView {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
+            int subscription = DEFAULT_SUB;
             if (Telephony.Intents.SPN_STRINGS_UPDATED_ACTION.equals(action)) {
-                updateNetworkName(intent.getBooleanExtra(Telephony.Intents.EXTRA_SHOW_SPN, false),
-                        intent.getStringExtra(Telephony.Intents.EXTRA_SPN),
-                        intent.getBooleanExtra(Telephony.Intents.EXTRA_SHOW_PLMN, false),
-                        intent.getStringExtra(Telephony.Intents.EXTRA_PLMN));
+                subscription = intent.getIntExtra(MSimConstants.SUBSCRIPTION_KEY, subscription);
+                if (subscription == mSubscription) {
+                    updateNetworkName(intent.getBooleanExtra(Telephony.Intents.EXTRA_SHOW_SPN, false),
+                            intent.getStringExtra(Telephony.Intents.EXTRA_SPN),
+                            intent.getBooleanExtra(Telephony.Intents.EXTRA_SHOW_PLMN, false),
+                            intent.getStringExtra(Telephony.Intents.EXTRA_PLMN));
+                }
             }
         }
     };
