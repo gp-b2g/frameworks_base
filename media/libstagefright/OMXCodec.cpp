6aedc308cb773913e5f357a16b9b5f4c36e45de8
Linux Build Service Account <lnxbuild@localhost>
2012-04-07 03:53:45 -0700

    Merge "libstagefright: Fix video encode I-frame interval setting" into ics

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1789,6 +1789,7 @@ status_t OMXCodec::setupMPEG4EncoderParameters(const sp<MetaData>& meta) {
         mpeg4type.nAllowedPictureTypes |= OMX_VIDEO_PictureTypeB;
         mpeg4type.nBFrames = 1;
         mNumBFrames = 1;
+        mpeg4type.nPFrames = mpeg4type.nPFrames / 2;
     }
 
     err = mOMX->setParameter(
@@ -1863,6 +1864,7 @@ status_t OMXCodec::setupAVCEncoderParameters(const sp<MetaData>& meta) {
         h264type.nPFrames = setPFramesSpacing(iFramesInterval, frameRate / hfrRatio);
         h264type.nBFrames = 1;
         mNumBFrames = 1;
+        h264type.nPFrames = h264type.nPFrames / 2;
     }
 
     if (h264type.nBFrames != 0) {
