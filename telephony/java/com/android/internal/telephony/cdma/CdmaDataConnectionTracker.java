c0964ab246770a289ff179d821c642abcff5c719
Linux Build Service Account <lnxbuild@localhost>
2012-04-11 11:11:26 -0700

    Merge "Telephony: Notify data connection fail in case of CDMA" into ics

--- telephony/java/com/android/internal/telephony/cdma/CdmaDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/cdma/CdmaDataConnectionTracker.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011-2012 Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -267,6 +267,10 @@ public class CdmaDataConnectionTracker extends DataConnectionTracker {
             notifyOffApnsOfAvailability(reason);
             return retValue;
         } else {
+            if (!mRequestedApnType.equals(Phone.APN_TYPE_DEFAULT)
+                    && (mState == State.IDLE || mState == State.SCANNING)) {
+                mPhone.notifyDataConnectionFailed(reason, mRequestedApnType);
+            }
             notifyOffApnsOfAvailability(reason);
             return false;
         }
