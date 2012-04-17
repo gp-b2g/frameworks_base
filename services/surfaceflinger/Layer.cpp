5347ceb64d0e9ed93d0999b4b7466bd82e8a9552
Linux Build Service Account <lnxbuild@localhost>
2012-04-16 22:07:31 -0700

    Merge "Surfaceflinger: Add Src orientation for the layer" into ics

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -41,7 +41,7 @@
 #include <qcom_ui.h>
 
 #define DEBUG_RESIZE    0
-
+#define SHIFT_SRC_TRANSFORM 4
 
 namespace android {
 
@@ -227,6 +227,10 @@ void Layer::setGeometry(hwc_layer_t* hwcl)
         hwcl->flags = HWC_SKIP_LAYER;
     } else {
         hwcl->transform = finalTransform;
+        //mBufferTransform will have the srcTransform
+        //include src and final transform in the hwcl->transform
+        hwcl->transform = (( bufferOrientation.getOrientation() <<
+                                       SHIFT_SRC_TRANSFORM) | hwcl->transform);
     }
 
     if (isCropped()) {
