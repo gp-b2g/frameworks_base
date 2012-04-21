e6f19c5ebc465704e29e551cf8ba1d9018813d70
Anshul Jain <anshulj@codeaurora.org>
2012-04-20 18:15:44 -0700

    Telephony: Ignore messages in CdmaLteSS tracker after dispose
    Change-Id: I34dbcea140629cd0c97c12c6ebd0ce2e08e53657
    CRs-Fixed: 350739

--- telephony/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -210,6 +210,8 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
     }
 
     public void dispose() {
+        log("ServiceStateTracker dispose");
+
         // Unregister for all events.
         cm.unregisterForRadioStateChanged(this);
         cm.unregisterForVoiceNetworkStateChanged(this);
