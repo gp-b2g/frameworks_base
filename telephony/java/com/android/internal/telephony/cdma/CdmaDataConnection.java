1062eb81dd7add2788d9170bb32620448f5a1672
Linux Build Service Account <lnxbuild@localhost>
2012-05-03 14:04:32 -0700

    Merge "Telephony: Enable support to use OMH data profiles" into ics

--- telephony/java/com/android/internal/telephony/cdma/CdmaDataConnection.java
+++ telephony/java/com/android/internal/telephony/cdma/CdmaDataConnection.java
@@ -21,6 +21,7 @@ import android.util.Log;
 
 import com.android.internal.telephony.DataConnection;
 import com.android.internal.telephony.DataProfile;
+import com.android.internal.telephony.DataProfile.DataProfileType;
 import com.android.internal.telephony.DataConnectionTracker;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.RILConstants;
@@ -87,14 +88,18 @@ public class CdmaDataConnection extends DataConnection {
             dataProfile = RILConstants.DATA_PROFILE_DEFAULT;
         }
 
-        mApn.setProfileId(dataProfile);
+        if (cp.apn.getDataProfileType() == DataProfileType.PROFILE_TYPE_OMH) {
+            dataProfile = cp.apn.getProfileId() + RILConstants.DATA_PROFILE_OEM_BASE;
+        } else {
+            mApn.setProfileId(dataProfile);
+        }
 
         // msg.obj will be returned in AsyncResult.userObj;
         Message msg = obtainMessage(EVENT_SETUP_DATA_CONNECTION_DONE, cp);
         msg.obj = cp;
         phone.mCM.setupDataCall(
                 Integer.toString(getRadioTechnology(RILConstants.SETUP_DATA_TECH_CDMA)),
-                Integer.toString(mApn.getProfileId()),
+                Integer.toString(dataProfile),
                 mApn.apn, mApn.user, mApn.password,
                 Integer.toString(mApn.authType),
                 mApn.protocol, msg);
