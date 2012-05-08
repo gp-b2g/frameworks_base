eacde4e0fb96b65efdc921099b05ec24596722b5
Linux Build Service Account <lnxbuild@localhost>
2012-05-07 19:35:08 -0700

    Merge "Tethering: Handle IllegalStateException on add/remove upstream v6 i/f" into ics

--- services/java/com/android/server/connectivity/Tethering.java
+++ services/java/com/android/server/connectivity/Tethering.java
@@ -1328,8 +1328,8 @@ public class Tethering extends INetworkManagementEventObserver.Stub {
                 Log.d(TAG, "adding v6 interface " + iface);
                 try {
                     service.addUpstreamV6Interface(iface);
-                } catch (RemoteException e) {
-                    Log.e(TAG, "Unable to append v6 upstream interface");
+                } catch (Exception e) {
+                    Log.e(TAG, "Unable to append v6 upstream interface", e);
                 }
             }
 
@@ -1340,8 +1340,8 @@ public class Tethering extends INetworkManagementEventObserver.Stub {
                 Log.d(TAG, "removing v6 interface " + iface);
                 try {
                     service.removeUpstreamV6Interface(iface);
-                } catch (RemoteException e) {
-                    Log.e(TAG, "Unable to remove v6 upstream interface");
+                } catch (Exception e) {
+                    Log.e(TAG, "Unable to remove v6 upstream interface", e);
                 }
             }
 
@@ -1552,7 +1552,7 @@ public class Tethering extends INetworkManagementEventObserver.Stub {
                             try {
                                 LinkProperties props = cm.getLinkProperties(info.getType());
                                 removeUpstreamV6Interface(props.getInterfaceName());
-                            } catch(RemoteException e) {
+                            } catch(Exception e) {
                                 Log.e(TAG, "Exception querying ConnectivityManager", e);
                             }
                         }
